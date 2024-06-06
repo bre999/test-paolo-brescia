39762057385ca6445248f9e95d6da83d79313716
bre999 <paolobre99@icloud.com>
2024-06-06 12:03:37 +0200

    Creazione componenti da cli

---
+++ test-paolo-brescia/src/app/components/pages/access-deined/access-deined.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-access-deined',
+  standalone: true,
+  imports: [],
+  templateUrl: './access-deined.component.html',
+  styleUrl: './access-deined.component.scss'
+})
+export class AccessDeinedComponent {
+
+}
