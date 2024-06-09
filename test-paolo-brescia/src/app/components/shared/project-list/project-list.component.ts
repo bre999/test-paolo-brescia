f2afaf537548c4f7eb56b8cc293b78be20ba505b
bre999 <paolobre99@icloud.com>
2024-06-09 04:38:17 +0200

    implementata la progress bar

--- test-paolo-brescia/src/app/components/shared/project-list/project-list.component.ts
+++ test-paolo-brescia/src/app/components/shared/project-list/project-list.component.ts
@@ -5,10 +5,12 @@ import { TaskListComponent } from '../task-list/task-list.component';
 import { ProjectService } from '../../../services/project.service';
 import { Project } from '../../../models/project.model';
 import { AuthService } from '../../../services/auth.service';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { User } from '../../../models/user.model';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MatProgressBar } from '@angular/material/progress-bar'
+import { TaskService } from '../../../services/task.service';
 
 
 @Component({
@@ -16,14 +18,14 @@ import { MatIconModule } from '@angular/material/icon';
   standalone: true,
   templateUrl: './project-list.component.html',
   styleUrls: ['./project-list.component.scss'],
-  imports: [CommonModule, TaskListComponent, MatCardModule, MatIconModule]
+  imports: [CommonModule, TaskListComponent, MatCardModule, MatIconModule, MatProgressBar]
 })
 export class ProjectListComponent implements OnInit {
   currentUser$: Observable<User | null>;
   projects$: Observable<Project[]>;
   selectedProjectId: string | null = null;
 
-  constructor(private projectService: ProjectService, private authService: AuthService) {
+  constructor(private projectService: ProjectService, private authService: AuthService, private taskService: TaskService) {
     this.projects$ = this.projectService.getProjects();
     this.currentUser$ = this.authService.getUser();
   }
@@ -35,6 +37,12 @@ export class ProjectListComponent implements OnInit {
 
   addProject() {
     this.projectService.addProject('name', 'desc');
+    console.log('percentage');
+    
+    this.getCompletionPercentage(this.selectedProjectId!).subscribe(console.log)
+    console.log('tasks');
+
+    this.taskService.getTasks(this.selectedProjectId!).subscribe(console.log)
   }
 
   deleteProject(projectId: string) {
@@ -50,6 +58,19 @@ export class ProjectListComponent implements OnInit {
   }
 
   editProject(project_id: string) { }
+  getCompletionPercentage(projectId: string): Observable<number> {
+    return this.taskService.getTasks(projectId).pipe(
+      map(tasks => {
+        if (!tasks || tasks.length === 0 || tasks.filter(task => task.completed).length === 0) {
+          // Se non ci sono task, ritorna 0
+          return 0;
+        }
+        // Calcola il numero di task completati
+        let completedTasks = tasks.filter(task => task.completed).length;
+        // Ritorna la percentuale di task completati
+        return (completedTasks / tasks.length) * 100;
+      })
+    );
+  }
 }
-
 
